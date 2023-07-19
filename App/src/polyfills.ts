bff8133b6e6310016383253e5418ebc47e5acf4c
HenryPham <phamtiensy8696@gmail.com>
2023-07-19 22:47:31 +0700

    11-12

--- App/src/polyfills.ts
+++ App/src/polyfills.ts
@@ -59,7 +59,7 @@ import '@angular/localize/init';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 // (window as any).global = window;
 // global.Buffer = global.Buffer || require('buffer').Buffer;
 // global.process = require('process');
